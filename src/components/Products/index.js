74d3275c98b6abd584f958e500362a60efae90d2
sriram <sriramsriram2316@gmail.com>
2024-09-26 08:51:25 +0000

    commit messge

---
+++ src/components/Products/index.js
@@ -0,0 +1,17 @@
+// Write your JS code here
+import Header from '../Header'
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="card-con">
+      <img
+        className="product-img"
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png "
+        alt="products"
+      />
+    </div>
+  </>
+)
+export default Products
